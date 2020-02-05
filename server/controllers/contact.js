fd5dcc5c4b725952dfde312c265f32e58b545b9b
helgasitanggang93 <helga.sitanggang93@gmail.com>
2020-02-05 13:53:38 +0700

    adding Contact Controller

--- server/controllers/contact.js
+++ server/controllers/contact.js
@@ -1,19 +1,20 @@
 const Contact = require('../models/contact')
 
 class ContactController {
-  static create(req, res, next){
+  static create(req, res, next) {
     const {
-      fullName, 
-      address, 
-      image, 
-      phoneNumber, 
-      createdBy} = req.body
+      fullName,
+      address,
+      image,
+      phoneNumber,
+      createdBy } = req.body
 
-      Contact.create({
-        fullName, 
-        address, 
-        image, 
-        phoneNumber, 
+    Contact
+      .create({
+        fullName,
+        address,
+        image,
+        phoneNumber,
         createdBy
       })
       .then((data) => {
@@ -22,21 +23,73 @@ class ContactController {
       .catch(next)
   }
 
-  static readAll(req, res, next){
-
+  static readAll(req, res, next) {
+    const { createdBy } = req.body
+    Contact.find({ createdBy })
+      .then(data => {
+        res.status(200).json(data)
+      })
+      .catch(next)
   }
 
-  static readOne(req, res, next){
+  static readOne(req, res, next) {
+    const { id } = req.params
 
+    Contact
+      .findOne({ _id: id })
+      .then(data => {
+        res.status(200).json(data)
+      })
+      .catch(next)
   }
 
   static update(req, res, next) {
+    const { id } = req.params
+    const {
+      fullName,
+      address,
+      image,
+      phoneNumber, } = req.body
 
+    let obj = {
+      fullName,
+      address,
+      image,
+      phoneNumber,
+    }
+
+    for (const key in obj) {
+      if (obj[key] === undefined) {
+        delete obj[key]
+      }
+    }
+
+    if (Object.keys(obj).length === 0) {
+      next({ status: 401, message: 'Nothing Change' })
+    } else {
+      Contact
+        .findOneAndUpdate({ _id: id }, obj)
+        .then(() => {
+
+          return Contact.findOne({ _id: id })
+        })
+        .then(data => {
+          res.status(200).json(data)
+        })
+        .catch(next)
+    }
   }
 
   static delete(req, res, next) {
+    const {id} = req.params
+         Portofolio
+         .findOneAndDelete({_id: id})
+         .then(data => {
+            res.status(201).json(data)
+         })
+         .catch(next)
 
-  }
+    }
 }
 
 module.exports = ContactController
--- server/routes/contact.js
+++ server/routes/contact.js
@@ -1,3 +1,10 @@
-const router = require('express').Router()
+const router = require('express').Router();
+const ContactController = require('../controllers/contact');
+const {authenticationCommon, authenticationContact} = require('../middlewares/authentication');
+const authorization = require('../middlewares/authorization');
 
+router.post('/', authenticationContact, ContactController.create);
+router.get('/', authenticationContact, ContactController.readAll);
+router.get('/:id', authenticationCommon, ContactController.readOne);
+router.patch('/:id', authenticationCommon, authorization, ContactController.update);
 module.exports = router
